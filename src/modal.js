86bddbf7bf8da6619616044a394c794e763c0e7e
NikitaPron <maxim.sasai@gmail.com>
2022-02-19 09:45:30 +0300

    scroll-modal-off

--- src/modal.js
+++ src/modal.js
@@ -9,17 +9,20 @@ document.addEventListener('DOMContentLoaded', () => {
     btnModalOpen.addEventListener('click', () => {
         modalWindow.style.visibility = 'visible';
         modalWindow.style.opacity = '100';
+        document.body.style.overflow = 'hidden';
     })
     
     btnModalClose.addEventListener('click', () => {
         modalWindow.style.visibility = 'hidden';
         modalWindow.style.opacity = '0';
+        document.body.style.overflow = 'auto';
     })
     
     document.addEventListener('click', (event) => {
         if(event.target.id === 'modal__area') {
             modalWindow.style.visibility = 'hidden';
             modalWindow.style.opacity = '0';
+            document.body.style.overflow = 'auto';
         }
     })
     
